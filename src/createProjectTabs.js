8fa254ac2412654b7551e73ca602db8d254144de
Abhinav Thota <59943893+abeeto@users.noreply.github.com>
2024-12-20 18:34:09 +0000

    new file to render names of all projects

---
+++ src/createProjectTabs.js
@@ -0,0 +1,26 @@
+import projects from "./ProjectsList";
+import { pubsub } from "./pubsub";
+function createProjectTabs() {
+
+    const projectTabHolderNode = document.createElement("div");
+    projectTabHolderNode.classList.add("project-tabs-wrapper");
+
+    const renderProjectTab = (name) => {
+        const projectTab = document.createElement("div");
+        projectTab.classList.add("project-tab");
+        projectTab.innerText = name;
+        return projectTab;
+    }
+
+    const renderProjectTabs = () => {
+        const allProjectNames = projects.getAllNamesOfProjects();
+        const projectTabs = allProjectNames.map(name => renderProjectTab(name));
+        projectTabHolderNode.replaceChildren(...projectTabs);
+    }
+
+    pubsub.subscribe("potentialNewProject", renderProjectTabs);
+    return projectTabHolderNode;
+}
+
+
+export default createProjectTabs();
