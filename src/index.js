4f6dc0039f6321b6bfa1a65cf36c85f77cca6149
Abhinav Thota <59943893+abeeto@users.noreply.github.com>
2024-12-23 16:50:48 +0000

    Render the task view of All Projects as default

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import CreateTaskForm from "./CreateTaskForm";
 import projectTabHolderNode from "./createProjectTabs";
 import { pubsub } from "./pubsub";
+import { renderTasks } from "./createTaskViewByProject";
+
 const bodyNode = document.querySelector("body");
 
 pubsub.publish("potentialNewProject", "All Projects")
+
 bodyNode.appendChild(CreateTaskForm);
 bodyNode.appendChild(projectTabHolderNode);
+
+renderTasks("All Projects");
