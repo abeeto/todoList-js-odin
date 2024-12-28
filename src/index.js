acce50f0720ee223138e210fc6420d5c66838d4e
Abhinav Thota <59943893+abeeto@users.noreply.github.com>
2024-12-28 12:38:08 +0000

    Style website:set backgrounds and center divs

--- src/index.js
+++ src/index.js
@@ -5,8 +5,13 @@ import { renderTasks } from "./createTaskViewByProject";
 import './style.css';
 
 const bodyNode = document.querySelector("body");
-bodyNode.appendChild(CreateTaskForm);
-bodyNode.appendChild(projectTabHolderNode);
+const mainWrapperNode = document.createElement("div");
+mainWrapperNode.classList.add("main-wrapper");
+
+mainWrapperNode.appendChild(CreateTaskForm);
+mainWrapperNode.appendChild(projectTabHolderNode);
+
+bodyNode.appendChild(mainWrapperNode);
 
 pubsub.publish("potentialNewProject", "All Projects")
 renderTasks("All Projects");
