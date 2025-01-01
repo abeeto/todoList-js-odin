053541a7b60e6ba49079c1b4775903339c0ef673
Abhinav Thota <59943893+abeeto@users.noreply.github.com>
2025-01-01 18:32:43 +0000

    rework DOM logic  of toggleTaskDoneButton; uses CSS styles instead of toggling SVGs

--- src/createTaskElement.js
+++ src/createTaskElement.js
@@ -3,20 +3,6 @@ import createTaskView from "./createTaskViewByProject";
 import ElementsHelper from "./ElementsHelper";
 import createEditableTaskView from "./createEditableTaskView";
 
-function createToggleTaskDoneButton(taskObj) {
-    const toggleTaskDoneButtonNode = document.createElement("button");
-    toggleTaskDoneButtonNode.classList.add("task__button", "task__button_toDo"); 
-    const undoneTaskIconSVG ='<svg clip-rule="evenodd" fill-rule="evenodd" stroke-linejoin="round" stroke-miterlimit="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path fill="#c7c7c7" d="m11.998 2.005c5.517 0 9.997 4.48 9.997 9.997 0 5.518-4.48 9.998-9.997 9.998-5.518 0-9.998-4.48-9.998-9.998 0-5.517 4.48-9.997 9.998-9.997zm-5.049 10.386 3.851 3.43c.142.128.321.19.499.19.202 0 .405-.081.552-.242l5.953-6.509c.131-.143.196-.323.196-.502 0-.41-.331-.747-.748-.747-.204 0-.405.082-.554.243l-5.453 5.962-3.298-2.938c-.144-.127-.321-.19-.499-.19-.415 0-.748.335-.748.746 0 .205.084.409.249.557z" fill-rule="nonzero"/></svg>';
-    const completedTaskIconSVG = '<svg clip-rule="evenodd" fill-rule="evenodd" stroke-linejoin="round" stroke-miterlimit="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path fill="#ba4949" d="m11.998 2.005c5.517 0 9.997 4.48 9.997 9.997 0 5.518-4.48 9.998-9.997 9.998-5.518 0-9.998-4.48-9.998-9.998 0-5.517 4.48-9.997 9.998-9.997zm-5.049 10.386 3.851 3.43c.142.128.321.19.499.19.202 0 .405-.081.552-.242l5.953-6.509c.131-.143.196-.323.196-.502 0-.41-.331-.747-.748-.747-.204 0-.405.082-.554.243l-5.453 5.962-3.298-2.938c-.144-.127-.321-.19-.499-.19-.415 0-.748.335-.748.746 0 .205.084.409.249.557z" fill-rule="nonzero"/></svg>';
-    toggleTaskDoneButtonNode.innerHTML = taskObj.getIsDone() ? completedTaskIconSVG : undoneTaskIconSVG;
-    toggleTaskDoneButtonNode.addEventListener("click", function() {
-        this.dataset.isDone =  this.dataset.isDone === "true" ? "false" : "true";
-        taskObj.toggleIsDone(this.dataset.isDone === "true");
-        console.log(taskObj.getIsDone())
-        this.innerHTML = taskObj.getIsDone() ? completedTaskIconSVG : undoneTaskIconSVG;
-    })
-    return toggleTaskDoneButtonNode;
-}
 export default function createTaskElement({taskObj, projectName}){
     const taskHolderNode = ElementsHelper.createGenericElement({elementTagName: "div", classList: ["wrapper", "wrapper-task", "flow-y-bottom", "flex"]})
     const taskInfoItemsHolder = ElementsHelper.createGenericElement({elementTagName: "div", classList: ["wrapper", "task__main-content", "flex", "flex-col"]})
@@ -26,7 +12,6 @@ export default function createTaskElement({taskObj, projectName}){
     const taskDueDateNode = ElementsHelper.createGenericElement({elementTagName: "div", classList: ["flex"], innerText: "Due Date: " + taskObj.getDueDate()});
     
     const taskDueInfoNode = ElementsHelper.createGenericElement({elementTagName: "div", classList: ["flex", "flex-space-apart", "taskBaseText", "flow-bottom-y"]});
-    taskDueInfoNode.replaceChildren(...[taskPriorityNode, taskDueDateNode]);
 
     const taskActionsNode = ElementsHelper.createGenericElement({elementTagName: "div", classList: ["flex", "flex-space-apart", "taskBaseText", "flow-bottom-y"]})
     const updateTaskActionNodes = ElementsHelper.createGenericElement({elementTagName: "div", classList: ["flex", "flow-bottom-y"]});
@@ -48,11 +33,20 @@ export default function createTaskElement({taskObj, projectName}){
         }, 
     })
 
-
-    const toggleTaskDoneButtonNode = createToggleTaskDoneButton(taskObj);
+    const toggleTaskDoneButtonNode = ElementsHelper.createGenericElement(
+        {        
+        elementTagName: "button",
+        classList: ["task__button", "task__button_toDo"],
+        clickEventCallBack: function() {
+            this.dataset.isDone =  this.dataset.isDone === "true" ? "false" : "true";
+            taskObj.toggleIsDone(this.dataset.isDone === "true");
+        }}
+    )
+    
     let hideableNodes = [taskDescInfoNode, deleteTaskButton, editTaskButton, toggleTaskDoneButtonNode];
     hideableNodes.forEach(node => node.dataset.isHidden = "true");
     toggleTaskDoneButtonNode.dataset.isHidden = "false";
+
     const expandButtonNode = ElementsHelper.createGenericElement({
         elementTagName: "button",
         innerText: "Expand",
@@ -61,7 +55,8 @@ export default function createTaskElement({taskObj, projectName}){
             this.innerText = taskDescInfoNode.dataset.isHidden === "true" ? "Expand" : "Hide";
         }
     })
-    
+
+    taskDueInfoNode.replaceChildren(...[taskPriorityNode, taskDueDateNode]);
     updateTaskActionNodes.replaceChildren(...[editTaskButton, deleteTaskButton])
     taskActionsNode.replaceChildren(...[expandButtonNode, updateTaskActionNodes]);
     taskInfoItemsHolder.replaceChildren(...[taskTitleInfoNode, taskDueInfoNode, taskDescInfoNode, taskActionsNode]);
