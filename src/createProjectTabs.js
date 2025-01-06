1a97aee0d9d39ae0f4881a3b29f0c4f4874355cb
Abhinav Thota <59943893+abeeto@users.noreply.github.com>
2025-01-06 00:50:13 +0000

    More specific event listener on projectTabs to prevent error, aesthetic changes and "No Projects" message on the tab holder

--- src/createProjectTabs.js
+++ src/createProjectTabs.js
@@ -4,36 +4,47 @@ import CreateTaskViewByProject from "./CreateTaskViewByProject";
 import ActiveProject from "./ActiveProject";
 import ElementsHelper from "./ElementsHelper";
 
+const projectTabHolderNode = document.createElement("div");
+
+const renderProjectTab = (name) => {
+  const projectTab = ElementsHelper.createGenericElement({
+    elementTagName: "div",
+    innerText: name,
+    attributesMap: { "data-project-name": name },
+    clickEventCallBack: (e) => {
+      const projectName = e.target.innerText;
+      ActiveProject.newActiveProject(e.target);
+      CreateTaskViewByProject.createTaskView(projectName);
+    },
+  });
+  return projectTab;
+};
+
+const renderProjectTabs = () => {
+  const allProjectNames = projects.getAllNamesOfProjects();
+  if (allProjectNames.length === 0) {
+    projectTabHolderNode.appendChild(
+      ElementsHelper.createGenericElement({
+        elementTagName: "div",
+        innerText: "No Projects Yet",
+        classList: ["empty-project-note"],
+      }),
+    );
+  } else {
+    const projectTabs = allProjectNames.map((name) => renderProjectTab(name));
+    projectTabHolderNode.replaceChildren(...projectTabs);
+  }
+};
+
 function createProjectTabs() {
-  const projectTabHolderNode = document.createElement("div");
   projectTabHolderNode.classList.add(
     "wrapper",
     "wrapper-projects",
     "flow-y-bottom",
   );
   projectTabHolderNode.id = "project-tabs-wrapper";
-  projectTabHolderNode.addEventListener("click", (e) => {
-    const projectName = e.target.innerText;
-    ActiveProject.newActiveProject(e.target);
-    CreateTaskViewByProject.createTaskView(projectName);
-  });
-
-  const renderProjectTab = (name) => {
-    const projectTab = ElementsHelper.createGenericElement({
-      elementTagName: "div",
-      innerText: name,
-      attributesMap: { "data-project-name": name },
-    });
-    return projectTab;
-  };
-
-  const renderProjectTabs = () => {
-    const allProjectNames = projects.getAllNamesOfProjects();
-    const projectTabs = allProjectNames.map((name) => renderProjectTab(name));
-    projectTabHolderNode.replaceChildren(...projectTabs);
-  };
-
   pubsub.subscribe("potentialNewProject", renderProjectTabs);
+  renderProjectTabs();
   return projectTabHolderNode;
 }
 
