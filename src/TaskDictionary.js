2d2e54127e55773b528f2985e7e1c52df5ce5606
Abhinav Thota <59943893+abeeto@users.noreply.github.com>
2025-01-08 08:44:40 +0000

    Add TaskDictionary: part of Task Object refactor

---
+++ src/TaskDictionary.js
@@ -0,0 +1,11 @@
+const TaskDictionary = {};
+
+const addTaskToDictionary = ({ id, taskObj }) => {
+  TaskDictionary[id] = taskObj;
+};
+
+const getTaskById = (id) => {
+  return TaskDictionary[id];
+};
+
+export default { addTaskToDictionary, getTaskById };
